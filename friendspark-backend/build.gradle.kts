002ee276df20dc72aed058e384469a6fb3157fb9
Evgenii Govorushkin <e.govorushkin@gmail.com>
2025-11-28 22:11:24 +0200

    feat: enhance user management with UserDetailsDTO and nearby user search functionality

--- friendspark-backend/build.gradle.kts
+++ friendspark-backend/build.gradle.kts
@@ -34,7 +34,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-security")
-//    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.google.firebase:firebase-admin:9.7.0")
