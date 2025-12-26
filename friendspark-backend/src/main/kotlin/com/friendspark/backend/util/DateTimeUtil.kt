43c72cbc6137204be8ec2b78603d028c333d5a2d
Evgenii Govorushkin <e.govorushkin@gmail.com>
2025-12-26 23:27:08 +0200

    feat: add DateTimeUtil component for retrieving current time using Clock

---
+++ friendspark-backend/src/main/kotlin/com/friendspark/backend/util/DateTimeUtil.kt
@@ -0,0 +1,15 @@
+package com.friendspark.backend.util
+
+import lombok.RequiredArgsConstructor
+import org.springframework.stereotype.Component
+import java.time.Clock
+import java.time.Instant
+
+@Component
+@RequiredArgsConstructor
+class DateTimeUtil(private val clock: Clock) {
+
+    fun now(): Instant? {
+        return this.clock.instant()
+    }
+}
